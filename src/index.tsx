77aa2cfa48c4fd309ac58b1f0d5b416fb4248470
cawauchi6204 <cawauchi6204@gmail.com>
2020-08-12 22:50:40 +0900

    1-4終了

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react"
+import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    return <h1 onClick="bar">Hello React!</h1>
+  return <h1 onClick="bar">Hello React!</h1>
 }
 
-ReactDOM.render(<App />,document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'))
